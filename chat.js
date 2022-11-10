349e126306f17bfc2453a1858f24c7ab8b7b1df4
digiguru <digiguru@gmail.com>
2022-11-10 22:39:00 +0000

    Adds different messages

--- chat.js
+++ chat.js
@@ -1,6 +1,7 @@
 const uuidv4 = require('uuid').v4;
 
 const messages = new Set();
+const gameState = "lobby";
 const users = new Map();
 
 const defaultUser = {
@@ -14,9 +15,28 @@ class Connection {
   constructor(io, socket) {
     this.socket = socket;
     this.io = io;
+    //Game state
+    socket.on('getGameState', () => this.getGameState());
+    socket.on('setGameState', (state) => this.handleSetGameState(state));
+    
+    //Users
+    socket.on('getUsers', () => this.getUsers());
+    socket.on('addUser', (user) => this.handleAddUser(user));
+    
 
+    //Prompts
+    socket.on('getPrompts', () => this.getUsers());
+    socket.on('addPrompt', (prompt) => this.handleAddPrompt(prompt));
+    
+    //images
+    socket.on('getImages', () => this.getUsers());
+     
+    
+    //Messages - DELETE
     socket.on('getMessages', () => this.getMessages());
     socket.on('message', (value) => this.handleMessage(value));
+
+    //General
     socket.on('disconnect', () => this.disconnect());
     socket.on('connect_error', (err) => {
       console.log(`connect_error due to ${err.message}`);
@@ -31,6 +51,13 @@ class Connection {
     messages.forEach((message) => this.sendMessage(message));
   }
 
+  getGameState() {
+    this.io.sockets.emit('gameState', gameState);
+  }
+  handleSetGameState(value) {
+    gameState = value;
+    this.getGameState();
+  }
   handleMessage(value) {
     const message = {
       id: uuidv4(),

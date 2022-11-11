038fe4cf962c8c67aad1ab0dd8f2a4a6838a0a8e
digiguru <digiguru@gmail.com>
2022-11-11 22:21:12 +0000

    Adds latest server code

--- chat.js
+++ chat.js
@@ -1,18 +1,33 @@
 const uuidv4 = require('uuid').v4;
 
 const messages = new Set();
-const gameState = "lobby";
+let gameState = "lobby";
 const users = new Map();
-
+//const prompts = new Map();
 const defaultUser = {
   id: 'anon',
   name: 'Anonymous',
 };
 
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function fakeWaitForServer() {
+  return new Promise(resolve => {
+    var length = randomIntFromInterval(1000,10000)
+    setTimeout(() => {
+      console.log("Waited for", length);
+      resolve( `did a wait for ${length}`);
+    }, length);
+  });
+}
 const messageExpirationTimeMS = 5*60 * 1000;
 
 class Connection {
   constructor(io, socket) {
+    console.log('calling');
+    //this.asyncCall();
     this.socket = socket;
     this.io = io;
     //Game state
@@ -25,12 +40,15 @@ class Connection {
     
 
     //Prompts
-    socket.on('getPrompts', () => this.getUsers());
+    //socket.on('getPrompts', () => this.getUsers());
     socket.on('addPrompt', (prompt) => this.handleAddPrompt(prompt));
     
     //images
     socket.on('getImages', () => this.getUsers());
-     
+
+    //vote
+    socket.on('getVotes', () => this.getVotes());
+    socket.on('addVote', (vote) => this.handleAddVote(vote));
     
     //Messages - DELETE
     socket.on('getMessages', () => this.getMessages());
@@ -42,7 +60,12 @@ class Connection {
       console.log(`connect_error due to ${err.message}`);
     });
   }
-  
+
+  async mockImageAPI() {
+    const result = await fakeWaitForServer();
+    return result;
+  }
+
   sendMessage(message) {
     this.io.sockets.emit('message', message);
   }
@@ -50,7 +73,7 @@ class Connection {
   getMessages() {
     messages.forEach((message) => this.sendMessage(message));
   }
-
+  ///Game state
   getGameState() {
     this.io.sockets.emit('gameState', gameState);
   }
@@ -58,10 +81,41 @@ class Connection {
     gameState = value;
     this.getGameState();
   }
+  ///Users
+  getUsers() {
+    this.io.sockets.emit('users', Array.from(users.values()));
+  }
+  handleAddUser(value) {
+    const user = {
+      id: uuidv4(),
+      value,
+      time: Date.now()
+    };
+    users.set(this.socket.id, user);
+    this.getUsers();
+  }
+
+  ///Prompt
+  getPrompt() {
+    //this.io.sockets.emit('users', Array.from(users.values()));
+  }
+  handleAddPrompt(prompt) {
+    console.log(prompt, Array.from(users.keys()), Array.from(users.values()), users)
+    var olduser = users.get(this.socket.id);
+    if(olduser && prompt && olduser.prompt !== prompt) {
+      var user = {...olduser, prompt};
+      users.set(this.socket.id, user);
+      this.getUsers();  
+      this.mockImageAPI().then((result) => {
+        console.log("API Called ", result)
+      });
+    }
+  }
+  
   handleMessage(value) {
     const message = {
       id: uuidv4(),
-      user: users.get(this.socket) || defaultUser,
+      user: users.get(this.socket.id) || defaultUser,
       value,
       time: Date.now()
     };
@@ -79,7 +133,7 @@ class Connection {
   }
 
   disconnect() {
-    users.delete(this.socket);
+    users.delete(this.socket.id);
   }
 }
 

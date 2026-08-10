3e94cbcc1b35083f83d40e4cee50cf50c26844f5
digiguru <digiguru@gmail.com>
2026-08-10 21:14:08 +0100

    fix(openai): restore DALL-E startup compatibility

--- dalle.js
+++ dalle.js
@@ -1,22 +1,31 @@
-const { Configuration, OpenAIApi } = require("openai");
-
-
+const OpenAI = require('openai');
 
 class Dalle {
-    constructor() {
-        this.configuration = new Configuration({
-            apiKey: process.env.DALLE_TOKEN,
-        });
-        this.openai = new OpenAIApi(this.configuration);
-    }
-    async promiseImage (prompt) {
-        return await this.openai.createImage({
-            prompt: prompt,
-            n: 1,
-            size: "512x512",
-          });
-    }
-}
+  constructor({ apiKey = process.env.DALLE_TOKEN, client } = {}) {
+    this.apiKey = apiKey;
+    this.openai = client;
+  }
 
+  getClient() {
+    if (!this.openai) {
+      if (!this.apiKey) {
+        throw new Error('DALLE_TOKEN is required to use the Dall-e generator');
+      }
+
+      this.openai = new OpenAI({ apiKey: this.apiKey });
+    }
+
+    return this.openai;
+  }
+
+  async promiseImage(prompt) {
+    return this.getClient().images.generate({
+      model: 'dall-e-2',
+      prompt,
+      n: 1,
+      size: '512x512',
+    });
+  }
+}
 
 module.exports = Dalle;
